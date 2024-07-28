4d4879e49cd1a10ae7b7fc106299f1b6486f8b42
mcaupybugs <vishalydv.me@gmail.com>
2024-07-28 19:47:45 +0530

    Adding the latest code to repo

---
+++ app/components/dinosaur.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dinosaur = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray-100 dinosaur-image-3'>
+    </div>
+  );
+};
+
+export default Dinosaur;
